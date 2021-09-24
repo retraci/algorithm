04942c3e65fa9bfba400d2ea4f770fd39b3326a1
retraci <korokseeds@foxmail.com>
2021-09-25 00:31:36 +0800

    09-24

--- template/prime factorization.cpp
+++ template/prime factorization.cpp
@@ -14,7 +14,7 @@ void divide(int n) {
     for (int i = 2; i <= sqrt(n); i++) {
         if (n % i == 0) {
             ps[++tt] = i, cs[tt] = 0;
-            while (n % i == 0) cs[tt]++;
+            while (n % i == 0) cs[tt]++, n /= i;
         }
     }
     if (n > 1) ps[++tt] = n, cs[tt] = 1;
