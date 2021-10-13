9fd474720dfe916afe8e56bd6f4065c06d6bc380
retraci <korokseeds@foxmail.com>
2021-10-13 21:16:12 +0800

    10-12

---
+++ bluebook/0x10 base data structures/0x14 hash/acwing_138.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+#define ull unsigned long long
+
+const int N = 1000010;
+const ull BASE = 131;
+
+int n, m;
+string s;
+ull h[N], p[N];
+
+ull get(int L, int R) {
+    return h[R] - h[L - 1] * p[R - L + 1];
+}
+
+void init() {
+    h[0] = 0, p[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        h[i] = h[i - 1] * BASE + s[i - 1] - 'a';
+        p[i] = p[i - 1] * BASE;
+    }
+}
+
+void solve() {
+    n = s.size();
+    init();
+
+    cin >> m;
+    while (m--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        ull hash1 = get(a, b), hash2 = get(c, d);
+        cout << (hash1 == hash2 ? "Yes" : "No") << endl;
+    }
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("../in.txt", "r", stdin);
+    freopen("../out.txt", "w", stdout);
+#endif
+
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    cin >> s;
+    solve();
+
+    return 0;
+}
