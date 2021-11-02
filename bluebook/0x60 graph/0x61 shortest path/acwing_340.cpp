5c832a22016f1222235e0007cf792beda1f3e143
retraci <korokseeds@foxmail.com>
2021-11-02 23:51:41 +0800

    11-02

---
+++ bluebook/0x60 graph/0x61 shortest path/acwing_340.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <queue>
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "NotAssignable"
+using namespace std;
+
+typedef pair<int, int> pii;
+
+const int N = 1010;
+
+int n, m, K;
+vector<pii> g[N];
+int dist[N];
+int vis[N];
+
+void add(int u, int v, int w) {
+    g[u].push_back({v, w});
+}
+
+bool check(int mid) {
+    memset(dist, 0x3f, sizeof dist);
+    memset(vis, 0, sizeof vis);
+    dist[1] = 0;
+    priority_queue<pii> que;
+    que.push({0, 1});
+    while (!que.empty()) {
+        auto [d, u] = que.top(); que.pop(); d = -d;
+        if (vis[u]) continue;
+        vis[u] = 1;
+
+        for (auto &[v, w]: g[u]) {
+            int nd = d + (w > mid);
+            if (dist[v] > nd) {
+                dist[v] = nd;
+                que.push({-dist[v], v});
+            }
+        }
+    }
+
+    return dist[n] <= K;
+}
+
+void solve() {
+    int left = 0, right = 1000001;
+    while (left < right) {
+        int mid = left + right >> 1;
+        if (check(mid)) right = mid;
+        else left = mid + 1;
+    }
+
+    cout << (left == 1000001 ? -1 : left) << endl;
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("../in.txt", "r", stdin);
+    freopen("../out.txt", "w", stdout);
+#endif
+
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cin >> n >> m >> K;
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        add(u, v, w), add(v, u, w);
+    }
+    solve();
+
+    return 0;
+}
+#pragma clang diagnostic pop
--- tmp.cpp
+++ tmp.cpp
@@ -1,43 +1,29 @@
-// #pragma GCC optimize(2)
-// #pragma GCC optimize(3)
-// #pragma GCC optimize("Ofast")
-#include<iostream>
-#include<cmath>
-#include<vector>
-#include<algorithm>
-#include<utility>
-#include<cstring>
-#include<iomanip>
-#include<unordered_set>
-#include<unordered_map>
-#include<set>
-#include<map>
-#include<queue>
-#include<numeric>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
-#define rep(i, x, y) for(int i = x; i < y; ++ i)
-#define _rep(i, x, y) for(int i = x; i > y; -- i)
-#define PII pair<int,int>
-#define eb emplace_back
-#define pb push_back
-#define fi first
-#define se second
-#define PQ priority_queue
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-typedef long long ll;
-typedef unsigned long long ull;
-typedef double db;
-typedef vector<int> VI;
-typedef vector<VI> VII;
-typedef vector<ll> VL;
-typedef vector<vector<ll>> VLL;
-constexpr int mod = 1e9 + 7;
-constexpr int KINF = 0x3f3f3f3f;
-constexpr int INF = 0x7f7f7f7f;
+
+string P, x;
+unordered_map<char, string> mp;
+
+void init() {
+    mp['0'] = "0000", mp['1'] = "0001", mp['2'] = "0010", mp['3'] = "0011";
+    mp['4'] = "0100", mp['5'] = "0101", mp['6'] = "0110", mp['7'] = "0111";
+    mp['8'] = "1000", mp['9'] = "1001", mp['A'] = "1010", mp['B'] = "1011";
+    mp['C'] = "1100", mp['D'] = "1101", mp['E'] = "1110", mp['F'] = "1111";
+}
+
+void solve() {
+    for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
+            cout << 1 << endl;
+        }
+    }
+}
 
 int main() {
 #ifdef LOCAL
@@ -45,89 +31,10 @@ int main() {
     freopen("../out.txt", "w", stdout);
 #endif
 
-    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    int _;
-    cin >> _;
-    unordered_map<char, string> mp;
-    mp['0'] = "0000";
-    mp['1'] = "0001";
-    mp['2'] = "0010";
-    mp['3'] = "0011";
-    mp['4'] = "0100";
-    mp['5'] = "0101";
-    mp['6'] = "0110";
-    mp['7'] = "0111";
-    mp['8'] = "1000";
-    mp['9'] = "1001";
-    mp['A'] = "1010";
-    mp['B'] = "1011";
-    mp['C'] = "1100";
-    mp['D'] = "1101";
-    mp['E'] = "1110";
-    mp['F'] = "1111";
-    while (_--) {
-        string p;
-        cin >> p;
-        string tmp;
-        cin >> tmp;
-        string s;
-        for (char &c: tmp) s += mp[c];
+    init();
 
-        cout << s << endl;
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    solve();
 
-        int i = 0;
-        while (s[i] == '0') i++;
-        s = s.substr(i, s.size() - i);
-        int k = p.size();
-        string ans;
-        auto del = [&](string f) {
-            for (int i = 0; i < f.size(); i++) {
-                if (f[i] == p[i]) f[i] = '0';
-                else f[i] = '1';
-            }
-            int i = 0;
-            while (i < f.size() && f[i] == '0') i++;
-            return f.substr(i, f.size() - i);
-        };
-        string cur = s.substr(0, k);
-        int j = k;
-        while (ans.size() < s.size() - 1) {
-            if (cur.size() < k) {
-                if (j < s.size()) {
-                    cur += s[j];
-                    ++j;
-                } else {
-                    cur += '0';
-                }
-                ans += '0';
-                continue;
-            }
-            //cout << cur << "#\n";
-            cur = del(cur);
-            //cout << cur << "-#\n";
-            ans += '1';
-            //if(ans.size() == s.size()) break;
-            if (j < s.size()) {
-                cur += s[j];
-                ++j;
-            } else {
-                cur += '0';
-            }
-            //cout << ans << "\n";
-        }
-        //cout << k << "\n";
-        //cout << cur << "\n";
-        while (cur.size() < k - 1) cur = "0" + cur;
-        if (cur.size() > k - 1) cur = cur.substr(0, k - 1);
-        cout << cur << "\n";
-    }
     return 0;
 }
-// 小心左移运算符导致的int溢出
-// dp记得处理非法状态
-// resize并不会初始化数组
-// 全局函数一定要记得inline
-// 带空格字符串不能直接cin,要用getline
-// 最好用"\n"代替endl
-
-
