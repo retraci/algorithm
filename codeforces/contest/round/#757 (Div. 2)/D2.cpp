f527d9cd2353f71d7cd4eba54749f6ba0b94cc3a
retraci <korokseeds@foxmail.com>
2021-12-04 20:03:53 +0800

    12-04

--- codeforces/contest/round/#757 (Div. 2)/D2.cpp
+++ codeforces/contest/round/#757 (Div. 2)/D2.cpp
@@ -13,18 +13,35 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
 const int N = 1e5 + 10;
-const int M = 5e6 + 10;
+const int M = 2e7 + 10;
 
 int n;
 int va[N];
 ll cnt[M];
 ll f[M];
 
+int vis[M];
+
+void primes(int lim) {
+    memset(vis, 0, sizeof vis);
+    vis[1] = 1;
+    for (int i = 2; i <= lim; i++) {
+        if (vis[i]) continue;
+        for (int j = i; j <= lim / i; j++) vis[i * j] = 1;
+    }
+}
+
 void solve() {
     int lim = *max_element(va + 1, va + n + 1);
 
+    primes(lim);
+    vector<int> ps;
+    for (int i = 2; i <= lim; i++) {
+        if (!vis[i]) ps.push_back(i);
+    }
+
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j * j <= va[i]; j++) {
+        for (int j = 1; 1LL * j * j <= va[i]; j++) {
             if (va[i] % j) continue;
             cnt[j]++; cnt[va[i] / j]++;
             if (j * j == va[i]) cnt[j]--;
@@ -33,7 +50,10 @@ void solve() {
 
     for (int i = lim; i >= 1; i--) {
         f[i] = i * cnt[i];
-        for (int j = 2 * i; j <= lim; j += i) {
+        for (int p : ps) {
+            ll j = 1LL * p * i;
+            if (j > lim) break;
+
             f[i] = max(f[i], f[j] + (cnt[i] - cnt[j]) * i);
         }
     }
--- template/math/prime factorization.cpp
+++ template/math/prime factorization.cpp
@@ -11,25 +11,25 @@ const int N = 1e5 + 10;
 
 ll ps[N], cs[N], tt;
 
-void divide(ll n) {
+void divide(ll x) {
     tt = 0;
-    for (ll i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) {
+    for (ll i = 2; 1LL * i * i <= x; i++) {
+        if (x % i == 0) {
             ps[++tt] = i, cs[tt] = 0;
-            while (n % i == 0) cs[tt]++, n /= i;
+            while (x % i == 0) cs[tt]++, x /= i;
         }
     }
-    if (n > 1) ps[++tt] = n, cs[tt] = 1;
+    if (x > 1) ps[++tt] = x, cs[tt] = 1;
 }
 
 // 埃筛
 int vis[N];
 
-void primes(int n) {
+void primes(int lim) {
     memset(vis, 0, sizeof vis);
     vis[1] = 1;
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= lim; i++) {
         if (vis[i]) continue;
-        for (int j = i; j <= n / i; j++) vis[i * j] = 1;
+        for (int j = i; j <= lim / i; j++) vis[i * j] = 1;
     }
 }
