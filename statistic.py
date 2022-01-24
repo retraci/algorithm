de7b46d7af690c9e2d6a6de97a090c62d12fc64b
retraci <korokseeds@foxmail.com>
2022-01-24 17:10:07 +0800

    add statisc script

---
+++ statistic.py
@@ -0,0 +1,63 @@
+import os
+
+root_path = r'E:\workspace\cpp\clion\algorithm'
+ignore_path = {os.path.join(root_path, '.idea'),
+               os.path.join(root_path, '.git'),
+               os.path.join(root_path, 'cmake-build-debug'),
+               os.path.join(root_path, 'template')}
+
+cpp_template_path = r'E:\workspace\cpp\clion\algorithm\template\tool\template.cpp'
+cpp_template = None
+
+
+def countLines(path):
+    assert os.path.exists(path)
+    try:
+        file = open(file=path, encoding='utf-8')
+        texts = file.readlines()
+
+        total_lines, valid_lines = 0, 0
+        for line in texts:
+            if not cpp_template.__contains__(line):
+                valid_lines += 1
+            total_lines += 1
+        return total_lines, valid_lines
+    except Exception as e:
+        print(e)
+        print(path)
+    finally:
+        return total_lines, valid_lines
+
+
+def countFiles(root_path):
+    assert os.path.exists(root_path)
+    total_files, total_lines, valid_lines = 0, 0, 0
+    item_list = os.listdir(root_path)
+    if len(item_list) == 0:
+        return total_files, total_lines, valid_lines
+    for item in item_list:
+        next_path = os.path.join(root_path, item)
+        if os.path.isfile(next_path):
+            if item[-4::] == '.cpp':
+                total_files += 1
+                ret1, ret2 = countLines(next_path)
+                total_lines += ret1
+                valid_lines += ret2
+        else:
+            if not ignore_path.__contains__(next_path):
+                ret1, ret2, ret3 = countFiles(next_path)
+                total_files += ret1
+                total_lines += ret2
+                valid_lines += ret3
+
+    return total_files, total_lines, valid_lines
+
+
+if __name__ == '__main__':
+    cpp_template_file = open(file=cpp_template_path, encoding='utf-8')
+    cpp_template = set(cpp_template_file.readlines())
+
+    total_files, total_lines, valid_lines = countFiles(root_path)
+    print(f'total files: {total_files}')
+    print(f'total lines: {total_lines}')
+    print(f'valid lines: {valid_lines}')
